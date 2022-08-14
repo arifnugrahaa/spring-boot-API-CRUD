ae003c02c155fe7629cc3d179307b8d7c2631200
arifnugrahaa <arifnugraha1243@gmail.com>
2022-08-14 14:58:06 +0700

    [addNewCar]

--- src/main/java/com/api/apicrud/Controller/CarController.java
+++ src/main/java/com/api/apicrud/Controller/CarController.java
@@ -1,15 +1,31 @@
 package com.api.apicrud.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.apicrud.Entities.CarEntity;
+import com.api.apicrud.Repositories.CarRepository;
 
 @RestController
 @RequestMapping( value = "/car")
 public class CarController {
     
+
+    @Autowired
+    CarRepository carRepository;
+
     @GetMapping(value = "checkAPI")
     public String checkAPI(){
         return "Hello World";
+    }
+
+    @PostMapping(value = "addNewCar")
+    public CarEntity addNewCar(@RequestBody CarEntity param){
+        carRepository.save(param);
+        return param;
     }
 }
---
+++ src/main/java/com/api/apicrud/Repositories/CarRepository.java
@@ -0,0 +1,11 @@
+package com.api.apicrud.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.apicrud.Entities.CarEntity;
+
+@Repository
+public interface CarRepository extends JpaRepository<CarEntity, Long> {
+    
+}
